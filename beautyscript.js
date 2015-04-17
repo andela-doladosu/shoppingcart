3a757fc2cf6a6d2d6c6d85d199f18fc5f229c5df
Oluwaseyi Talabi <oatalabi@gmail.com>
2015-04-17 11:00:16 +0100

    Update beautyscript.js

--- beautyscript.js
+++ beautyscript.js
@@ -4,8 +4,6 @@ var imageIndex=0;
 var thecart = document.querySelectorAll("#cart ul")[0];
 var total = 0;
 
-alert(localstorage.names);
-
 function changeImage(){
   myPhoto.setAttribute("src", imageArray[imageIndex]);
   imageIndex++;
